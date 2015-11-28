cda76354992eaa8823de5384888a91df0e987cbe
Len Smith <len@barrison.com>
2015-11-27 17:57:49 -0800

    Move VideoList properties inside of the constructor

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,6 +2,18 @@ import React from 'react';
 import Video from './video'
 
 class VideoList extends React.Component {
+  constructor() {
+    super()
+    this.propTypes = {
+      videos: React.PropTypes.array.isRequired
+    }
+
+    this.defaultProps = {
+      videos: [ { name: " Setting up your environment" },
+                { name: " Writing our first component" } ]
+    }
+  }
+
   renderVideos() {
     return this.props.videos.map((video) => {
       return <Video name={ video.name } />
@@ -15,14 +27,5 @@ class VideoList extends React.Component {
       </div>
   }
 }
-
-VideoList.defaultProps = {
-  videos: [ {name: " Setting up your environment" },
-            {name: " Writing our first component" } ]
-};
-
-VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
-};
 
 export default VideoList
