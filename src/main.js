bd1071044e2aebf10787aa91c545d1d084c4ae39
Len Smith <len@barrison.com>
2015-12-14 14:49:00 -0800

    Show video list at /

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import App from "../src/components/app"
 import videoStore from '../src/stores/video_store'
 import VideoListLoader from '../src/components/video_list_loader'
 import SignUpForm from '../src/components/sign_up_form'
-import { Router, Route } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
 
 render((
     <Provider store={videoStore}>
       <Router history={ history }>
-        <Route path="/" component={App}>
+        <Route path="/" component={ App }>
+          <IndexRoute component={ VideoListLoader } />
           <Route path="videos" component={VideoListLoader} />
           <Route path="register" component={SignUpForm} />
         </Route>
